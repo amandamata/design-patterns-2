f6a2f7a9c577c62da6ac12f5d521ed30e7f55868
Amanda <amanda.mata@pagar.me>
2022-05-30 08:38:21 -0300

    :alembic: wrong way to make connection in db

---
+++ DesignPatterns2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection connection = new SqlConnection();
+            connection.ConnectionString = "User Id=root;Password=;Server=localhost;Database=myDB";
+            connection.Open();
+            IDbCommand command = connection.CreateCommand();
+            command.CommandText = "select * from table";
+        }
+    }
+}
